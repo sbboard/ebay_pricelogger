fd3f693a7075cb5828b379133d2469ea37cbf828
sbboard <colin.buffum@gmail.com>
2019-05-08 17:49:29 -0600

    began crafting search query

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
 const axios = require('axios')
 const path = require('path')
 const config = require(path.join(__dirname, 'config.js'));
-var date = new Date();
-let yesterday = date.setDate(date.getDate() - 1);
+let date = new Date();
+console.log(date)
+let yesterdayMilli = date.setDate(date.getDate() - 14);
+let yesterday = new Date(yesterdayMilli)
+console.log(yesterday)
 
 const apiURL = "http://svcs.ebay.com/services/search/FindingService/v1?"+
 "OPERATION-NAME=findCompletedItems"+
 "&SECURITY-APPNAME="+config.client_id+
 "&RESPONSE-DATA-FORMAT=JSON"+
-"&keywords=Gameboy"+
-//"&categoryId=156955"+
+"&keywords=gameboy"+
+"&categoryId=139971"+
 "&itemFilter(0).name=Condition"+
-"&itemFilter(0).value=3000"+
-"&itemFilter(1).name=FreeShippingOnly"+
+"&itemFilter(0).value=7000"+
+"&itemFilter(1).name=SoldItemsOnly"+
 "&itemFilter(1).value=true"+
-"&itemFilter(2).name=SoldItemsOnly"+
-"&itemFilter(2).value=true"+
-"&sortOrder=PricePlusShippingLowest"
+"&itemFilter(2).name=EndTimeFrom"+
+"&itemFilter(2).value="+date+
+"&itemFilter(3).name=EndTimeTo"+
+"&itemFilter(3).value="+yesterday
 
 axios.get(apiURL)
   .then(function (response) {
     console.log(response.data.findCompletedItemsResponse[0].searchResult[0].item);
+    console.log(response.data.findCompletedItemsResponse[0].errorMessage[0].error[0].message)
   })
-//   .catch(function (error) {
-//     console.log(error);
-//   });
+  .catch(function (error) {
+    console.log(error);
+  });
 
 // setInterval(()=> {
 //     let today = new Date()
