e94563ad9386c4d3941b2f99b977382b8f00e47d
sbboard <colin.buffum@gmail.com>
2019-05-10 14:41:01 -0600

    sorted into subfolders for different functions

---
+++ API/routes/product.route.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+const product_controller = require('../controllers/product.controller')
+
+router.get('/:num', product_controller.person_info)
+
+router.get('/', product_controller.whole_list)
+
+module.exports = router
