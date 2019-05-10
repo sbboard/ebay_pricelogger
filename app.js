7801bd1fcad55815bbf3c4637052c7cc8549ed0c
sbboard <colin.buffum@gmail.com>
2019-05-09 23:48:38 -0600

    put into interval

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
 const axios = require('axios')
 const path = require('path')
 const config = require(path.join(__dirname, 'config.js'));
-let date = new Date();
-const today = date.toISOString()
-let yesterdayMilli = date.setDate(date.getDate() - 1);
-const yesterday = new Date(yesterdayMilli).toISOString()
-let gameboysOfToday = []
+const admin = {
+  interval = 24*60*1000
+}
 
-const apiURL = "http://svcs.ebay.com/services/search/FindingService/v1?"+
-"OPERATION-NAME=findCompletedItems"+
-"&SECURITY-APPNAME="+config.client_id+
-"&RESPONSE-DATA-FORMAT=JSON"+
-//`&keywords=%27gameboy color%27 %27for parts%27`+
-`&keywords=gameboy color for parts`+
-"&categoryId=139971"+
-"&itemFilter(0).name=Condition"+
-"&itemFilter(0).value=7000"+
-"&itemFilter(1).name=SoldItemsOnly"+
-"&itemFilter(1).value=true"+
-"&itemFilter(2).name=EndTimeFrom"+
-"&itemFilter(2).value="+yesterday+
-"&itemFilter(3).name=EndTimeTo"+
-"&itemFilter(3).value="+today
+setInterval(()=> {
+  let date = new Date();
+  const today = date.toISOString()
+  let yesterdayMilli = date.setDate(date.getDate() - 1);
+  const yesterday = new Date(yesterdayMilli).toISOString()
+  let gameboysOfToday = []
+  const apiURL = "http://svcs.ebay.com/services/search/FindingService/v1?"+
+  "OPERATION-NAME=findCompletedItems"+
+  "&SECURITY-APPNAME="+config.client_id+
+  "&RESPONSE-DATA-FORMAT=JSON"+
+  //`&keywords=%27gameboy color%27 %27for parts%27`+
+  `&keywords=gameboy color for parts`+
+  "&categoryId=139971"+
+  "&itemFilter(0).name=Condition"+
+  "&itemFilter(0).value=7000"+
+  "&itemFilter(1).name=SoldItemsOnly"+
+  "&itemFilter(1).value=true"+
+  "&itemFilter(2).name=EndTimeFrom"+
+  "&itemFilter(2).value="+yesterday+
+  "&itemFilter(3).name=EndTimeTo"+
+  "&itemFilter(3).value="+today
 
-axios.get(apiURL)
+  axios.get(apiURL)
   .then(function (response) {
     if(typeof response.data.findCompletedItemsResponse[0].searchResult != 'undefined'){
       let gameboy = response.data.findCompletedItemsResponse[0].searchResult[0].item
@@ -61,16 +65,4 @@ axios.get(apiURL)
   .catch(function (error) {
     console.log(error);
   });
-
-// setInterval(()=> {
-//     let today = new Date()
-//     if(today.getHours() == admin.timeToSend || admin.dayOverride == true){
-//         ebay.findCompletedItems("\"gameboy color\" \"for parts\"")
-//         .then((data) => {
-//             console.log(data)
-//         })
-//         .then(()=>{
-//             //theBoys.sort(({cost:a}, {cost:b}) => a-b);
-//         })
-//     }
-// }, admin.interval);
+}, admin.interval);
