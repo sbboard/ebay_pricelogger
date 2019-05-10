98caa5afe7a2c4795481ae3d5b800e5dbe92392d
sbboard <colin.buffum@gmail.com>
2019-05-09 23:05:13 -0600

    query and filter now returns correct results

--- app.js
+++ app.js
@@ -2,30 +2,35 @@ const axios = require('axios')
 const path = require('path')
 const config = require(path.join(__dirname, 'config.js'));
 let date = new Date();
-console.log(date)
-let yesterdayMilli = date.setDate(date.getDate() - 14);
-let yesterday = new Date(yesterdayMilli)
-console.log(yesterday)
+const today = date.toISOString()
+let yesterdayMilli = date.setDate(date.getDate() - 1);
+const yesterday = new Date(yesterdayMilli).toISOString()
 
 const apiURL = "http://svcs.ebay.com/services/search/FindingService/v1?"+
 "OPERATION-NAME=findCompletedItems"+
 "&SECURITY-APPNAME="+config.client_id+
 "&RESPONSE-DATA-FORMAT=JSON"+
-"&keywords=gameboy"+
+"&keywords=gameboy color for parts"+
 "&categoryId=139971"+
 "&itemFilter(0).name=Condition"+
 "&itemFilter(0).value=7000"+
 "&itemFilter(1).name=SoldItemsOnly"+
 "&itemFilter(1).value=true"+
 "&itemFilter(2).name=EndTimeFrom"+
-"&itemFilter(2).value="+date+
+"&itemFilter(2).value="+yesterday+
 "&itemFilter(3).name=EndTimeTo"+
-"&itemFilter(3).value="+yesterday
+"&itemFilter(3).value="+today
+
+console.log(apiURL)
 
 axios.get(apiURL)
   .then(function (response) {
-    console.log(response.data.findCompletedItemsResponse[0].searchResult[0].item);
-    console.log(response.data.findCompletedItemsResponse[0].errorMessage[0].error[0].message)
+    if(typeof response.data.findCompletedItemsResponse[0].searchResult != 'undefined'){
+      console.log(response.data.findCompletedItemsResponse[0].searchResult[0].item);
+    }
+    if(typeof response.data.findCompletedItemsResponse[0].errorMessage != 'undefined'){
+      console.log(response.data.findCompletedItemsResponse[0].errorMessage[0].error[0].message)
+    }
   })
   .catch(function (error) {
     console.log(error);
