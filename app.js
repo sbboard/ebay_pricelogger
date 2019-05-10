0e0f641dc696232a8448eec63ca8d765ded69344
sbboard <colin.buffum@gmail.com>
2019-05-09 23:44:27 -0600

    gameboysOfToday object created properly

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ let date = new Date();
 const today = date.toISOString()
 let yesterdayMilli = date.setDate(date.getDate() - 1);
 const yesterday = new Date(yesterdayMilli).toISOString()
+let gameboysOfToday = []
 
 const apiURL = "http://svcs.ebay.com/services/search/FindingService/v1?"+
 "OPERATION-NAME=findCompletedItems"+
 "&SECURITY-APPNAME="+config.client_id+
 "&RESPONSE-DATA-FORMAT=JSON"+
-"&keywords=gameboy color for parts"+
+//`&keywords=%27gameboy color%27 %27for parts%27`+
+`&keywords=gameboy color for parts`+
 "&categoryId=139971"+
 "&itemFilter(0).name=Condition"+
 "&itemFilter(0).value=7000"+
@@ -21,12 +23,36 @@ const apiURL = "http://svcs.ebay.com/services/search/FindingService/v1?"+
 "&itemFilter(3).name=EndTimeTo"+
 "&itemFilter(3).value="+today
 
-console.log(apiURL)
-
 axios.get(apiURL)
   .then(function (response) {
     if(typeof response.data.findCompletedItemsResponse[0].searchResult != 'undefined'){
-      console.log(response.data.findCompletedItemsResponse[0].searchResult[0].item);
+      let gameboy = response.data.findCompletedItemsResponse[0].searchResult[0].item
+      for(let i=0;i<gameboy.length;i++){
+        let thisBoy = {}
+        //Title
+        thisBoy.title = gameboy[i].title[0]
+        //Price
+        let price = parseFloat(gameboy[i].sellingStatus[0].convertedCurrentPrice[0].__value__).toFixed(2)
+        let shippingCost = 0
+        if(typeof gameboy[i].shippingInfo[0].shippingServiceCost != 'undefined'){
+            shippingCost = parseFloat(gameboy[i].shippingInfo[0].shippingServiceCost[0].__value__).toFixed(2)
+        }
+        else{
+            shippingCost = "*"
+        }
+        thisBoy.actualPrice = price + shippingCost
+        //URL
+        thisBoy.url = gameboy[i].viewItemURL[0]
+        //Date
+        thisBoy.dateSale = gameboy[i].listingInfo[0].endTime[0]
+        if(thisBoy.title.toLowerCase().includes("pocket") || thisBoy.title.toLowerCase().includes("lot")){
+          console.log("skip")
+        }
+        else{
+          gameboysOfToday.push(thisBoy)
+        }
+      }
+      console.log(gameboysOfToday)
     }
     if(typeof response.data.findCompletedItemsResponse[0].errorMessage != 'undefined'){
       console.log(response.data.findCompletedItemsResponse[0].errorMessage[0].error[0].message)
