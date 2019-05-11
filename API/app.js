45816579bbdd015f435ef372dcc4150edbc69b49
sbboard <colin.buffum@gmail.com>
2019-05-11 00:53:13 -0600

    got chart and axios working

--- API/app.js
+++ API/app.js
@@ -18,6 +18,14 @@ app.use(bodyParser.urlencoded({extended: false}))
 
 const product = require('./routes/product.route')
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.use('/', product)
 
 const port = (process.env.PORT || 8128)
