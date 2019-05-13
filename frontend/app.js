5626989667883410516129ec09b2d5501c23351d
sbboard <colin.buffum@gmail.com>
2019-05-12 20:30:12 -0600

    fixed problem with chart generation

--- frontend/app.js
+++ frontend/app.js
@@ -6,11 +6,10 @@ axios.get('https://gameboy-price-logger.herokuapp.com/')
 
 function sortData(data){
     data.sort(function (a, b) {
-        if (a.date > b.date) return 1;
-        if (a.date < b.date) return -1;
+        if (a.date > b.date) return 1
+        if (a.date < b.date) return -1
         return 0;
-      });
-    console.log(data)
+    })
     let currentDate = 0
     let currentCount = 0
     let currentTotal = 0
@@ -23,8 +22,8 @@ function sortData(data){
             }
             days.push(data[i].date.split("T")[0])
             currentDate = data[i].date.split("T")[0]
-            currentTotal = 0
-            currentCount = 0
+            currentTotal = parseFloat(data[i].price)
+            currentCount = 1
         }
         else{
             currentTotal += parseFloat(data[i].price)
@@ -34,7 +33,6 @@ function sortData(data){
     if(currentCount != 0){
         avs.push(currentTotal / currentCount)
     }
-    console.log(days, avs)
     createChart(days, avs)
 }
 
